3bcb3c59119ad70e937a2047d66fde34c69cce4a
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-01 09:23:57 +0530

    added assertion

--- Tests/DemoTests.cs
+++ Tests/DemoTests.cs
@@ -1,16 +1,16 @@
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
 using selenium_csharp_ui_automation_framework.Tests.TestSetup;
 
 namespace selenium_csharp_ui_automation_framework.Tests
 {
-    public class DemoTests :BaseTestSetup
+    public class DemoTests : BaseTestSetup
     {
        
         [Test]
         public void DemoTest()
         {
             _driver.Navigate().GoToUrl("https://www.google.com");
+            Console.WriteLine("Title : "+_driver.Title);
+            Assert.That(_driver.Title.Contains("Google"), "Title does not contain 'Google'");
         }
     }
 }

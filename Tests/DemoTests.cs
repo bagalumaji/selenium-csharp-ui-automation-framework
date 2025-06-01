df79942e7d5fcdf85864f23497ffc11f73831fc6
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-01 09:18:38 +0530

    updated test

--- Tests/DemoTests.cs
+++ Tests/DemoTests.cs
@@ -1,32 +1,16 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using selenium_csharp_ui_automation_framework.Tests.TestSetup;
 
 namespace selenium_csharp_ui_automation_framework.Tests
 {
-    public class DemoTests
+    public class DemoTests :BaseTestSetup
     {
-        private IWebDriver _driver = null;
-        [SetUp]
-        public void Setup()
-        {
-            _driver = new ChromeDriver();
-            _driver.Manage().Window.Maximize();
-        }
-
+       
         [Test]
         public void DemoTest()
         {
             _driver.Navigate().GoToUrl("https://www.google.com");
-
-        }
-        [TearDown]
-        public void TearDown()
-        {
-            if (_driver != null)
-            {
-                _driver.Quit();
-                _driver.Dispose();
-            }
         }
     }
 }

6e9d92c8af14bfa9c8d265bd4f1adeeaebc68cbe
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-02 13:17:56 +0530

    added click, sendkey methods

--- PageAction/PageActions.cs
+++ PageAction/PageActions.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using selenium_csharp_ui_automation_framework.DriverInfo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,26 @@ using System.Threading.Tasks;
 
 namespace selenium_csharp_ui_automation_framework.PageAction
 {
-    internal class PageActions
+    public class PageActions
     {
+        public static void NavigateToUrl(string url)
+        {
+           DriverManager.GetDriver().Navigate().GoToUrl(url);
+        }
+        public static void click(IWebElement element)
+        {
+           element.Click();
+        }
+        public static void sendKeys(IWebElement element, string text)
+        {
+            element.Clear();
+           element.SendKeys(text);
+        }
+
+        public static string getTitle()
+        {
+            return DriverManager.GetDriver().Title;
+        }
+
     }
 }

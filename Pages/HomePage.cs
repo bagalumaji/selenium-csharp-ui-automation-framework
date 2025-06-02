5e4afccda403b6bc52ad2846a8de7b87126c54a8
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-02 13:11:23 +0530

    created home page class and method for verification of page

---
+++ Pages/HomePage.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace selenium_csharp_ui_automation_framework.Pages
+{
+    public class HomePage : BasePage
+    {
+        By titleProduct = By.XPath("//span[normalize-space()='Products']");
+
+        public bool isTitleProductDisplayed()
+        {
+            try
+            {
+                return this.driver.FindElement(titleProduct).Displayed;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+        public void verifyThatTitleProductIsDisplayed()
+        {
+            Assert.That(isTitleProductDisplayed(), "Title 'Products' is not displayed on the page.");   
+        }
+
+    }
+}

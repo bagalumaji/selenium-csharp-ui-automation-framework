ecfbdae5d1a1487c9374f849f4fa4b0084fb2b0f
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-02 13:24:53 +0530

    added is displayed method

--- PageAction/PageActions.cs
+++ PageAction/PageActions.cs
@@ -24,5 +24,17 @@ namespace selenium_csharp_ui_automation_framework.PageAction
             return DriverManager.GetDriver().Title;
         }
 
+        public static bool isElementDisplayed(IWebElement element)
+        {
+            try
+            {
+                return element.Displayed;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
     }
 }

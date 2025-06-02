7df2f9ff640379a8fe2e2ae7f78782976f07e39b
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-02 12:32:03 +0530

    driver manager is updated with lambda expression

---
+++ Driver/DriverManger.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+
+namespace selenium_csharp_ui_automation_framework.DriverInfo
+{
+    public class DriverManger
+    {
+        private static ThreadLocal<IWebDriver> threadLocalDriver = new ThreadLocal<IWebDriver>();
+        public static IWebDriver GetDriver() => threadLocalDriver.Value;
+
+        public static void SetDriver(IWebDriver driver) => threadLocalDriver.Value = driver;
+        public static void UnloadDriver()
+        {
+            if (threadLocalDriver.IsValueCreated && threadLocalDriver.Value != null)
+            {
+                threadLocalDriver.Value.Dispose();
+                threadLocalDriver.Value = null;
+            }
+        }
+    }
+}

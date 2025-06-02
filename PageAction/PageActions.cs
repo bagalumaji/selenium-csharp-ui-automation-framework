e75e28efd923ef488ddbb47bc681f36235b3947c
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-02 13:20:52 +0530

    removed unsued using

--- PageAction/PageActions.cs
+++ PageAction/PageActions.cs
@@ -1,10 +1,5 @@
 ﻿using OpenQA.Selenium;
 using selenium_csharp_ui_automation_framework.DriverInfo;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace selenium_csharp_ui_automation_framework.PageAction
 {
@@ -12,16 +7,16 @@ namespace selenium_csharp_ui_automation_framework.PageAction
     {
         public static void NavigateToUrl(string url)
         {
-           DriverManager.GetDriver().Navigate().GoToUrl(url);
+            DriverManager.GetDriver().Navigate().GoToUrl(url);
         }
         public static void click(IWebElement element)
         {
-           element.Click();
+            element.Click();
         }
         public static void sendKeys(IWebElement element, string text)
         {
             element.Clear();
-           element.SendKeys(text);
+            element.SendKeys(text);
         }
 
         public static string getTitle()

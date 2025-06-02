538613920cf633b00f9815968605e2da92c0ed14
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-02 13:03:34 +0530

    added locator and methods for login

--- Pages/LoginPage.cs
+++ Pages/LoginPage.cs
@@ -1,7 +1,40 @@
-﻿namespace selenium_csharp_ui_automation_framework.Pages
-{
-    public class LoginPage
-    {
+﻿
+using OpenQA.Selenium;
 
+namespace selenium_csharp_ui_automation_framework.Pages
+{
+    public class LoginPage : BasePage
+    {
+        By UserNameTextBox = By.Id("user-name");
+        By PasswordTextBox = By.Id("password");
+        By LoginButton = By.Name("login-button");
+
+        public void clickLoginButton()
+        {
+            this.driver.FindElement(LoginButton).Click();
+        }
+
+        public void enterUserName(string userName)
+        {
+            this.driver.FindElement(UserNameTextBox).SendKeys(userName);
+        }
+        public void enterPassword(string password)
+        {
+            this.driver.FindElement(PasswordTextBox).SendKeys(password);
+        }
+        public void login(string userName, string password)
+        {
+            enterUserName(userName);
+            enterPassword(password);
+            clickLoginButton();
+        }
+        public bool isLoginButtonDisplayed()
+        {
+            return this.driver.FindElement(LoginButton).Displayed;
+        }
+        public void verifyThatLoginButtonIsDisplayed()
+        {
+            Assert.That(isLoginButtonDisplayed(), "Login button is not displayed on the page.");
+        }
     }
 }

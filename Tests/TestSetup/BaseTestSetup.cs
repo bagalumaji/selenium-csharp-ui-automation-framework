d7e9e721304cafda3ce43e8fe8ec41e9b0661890
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-02 12:32:11 +0530

    updated

--- Tests/TestSetup/BaseTestSetup.cs
+++ Tests/TestSetup/BaseTestSetup.cs
@@ -1,30 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
+﻿using selenium_csharp_ui_automation_framework.DriverInfo;
 
 namespace selenium_csharp_ui_automation_framework.Tests.TestSetup
 {
     public class BaseTestSetup
     {
-        protected IWebDriver _driver = null;
+
         [SetUp]
         public void Setup()
         {
-            _driver = new ChromeDriver();
-            _driver.Manage().Window.Maximize();
+            Driver.InitDriver();
         }
         [TearDown]
         public void TearDown()
         {
-            if (_driver != null)
-            {
-                _driver.Quit();
-                _driver.Dispose();
-            }
+            Driver.QuitDriver();
         }
     }
 }

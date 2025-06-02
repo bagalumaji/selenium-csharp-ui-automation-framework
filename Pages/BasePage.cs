53e3190397d22351befc337efc0a8e9d689c3c71
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-02 13:03:11 +0530

    updated base page with driver

--- Pages/BasePage.cs
+++ Pages/BasePage.cs
@@ -1,17 +1,13 @@
 ﻿using OpenQA.Selenium;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using selenium_csharp_ui_automation_framework.DriverInfo;
 namespace selenium_csharp_ui_automation_framework.Pages
 {
-    abstract internal class BasePage
+    abstract public class BasePage
     {
         protected IWebDriver driver;
-        protected BasePage() {
-           this.driver = DriverManager.GetDriver();
+        protected BasePage()
+        {
+            this.driver = DriverManager.GetDriver();
         }
     }
 }

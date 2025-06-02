9fb2cfd1904545e98ae20ed41f4773c9415e5b87
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-02 13:03:51 +0530

    updated test with page object methods

--- Tests/DemoTests.cs
+++ Tests/DemoTests.cs
@@ -1,4 +1,5 @@
 using selenium_csharp_ui_automation_framework.DriverInfo;
+using selenium_csharp_ui_automation_framework.Pages;
 using selenium_csharp_ui_automation_framework.Tests.TestSetup;
 
 namespace selenium_csharp_ui_automation_framework.Tests
@@ -9,10 +10,15 @@ namespace selenium_csharp_ui_automation_framework.Tests
         [Test]
         public void DemoTest()
         {
-            String title = "Swag Labs";
+            string title = "Swag Labs";
             DriverManager.GetDriver().Navigate().GoToUrl("https://www.saucedemo.com/");
             Console.WriteLine("Title : " + DriverManager.GetDriver().Title);
             Assert.That(DriverManager.GetDriver().Title.Contains(title), "Title does not contain "+title);
+
+            LoginPage loginPage = new LoginPage();
+            loginPage.verifyThatLoginButtonIsDisplayed();
+            loginPage.login("standard_user", "secret_sauce");
+            Thread.Sleep(5000);
         }
     }
 }

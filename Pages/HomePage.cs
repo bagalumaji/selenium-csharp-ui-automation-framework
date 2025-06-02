a348ab029d85474a2fa15b2edba3e3a03e39aec8
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-02 13:24:39 +0530

    updated with is displayed method

--- Pages/HomePage.cs
+++ Pages/HomePage.cs
@@ -1,4 +1,5 @@
 ﻿using OpenQA.Selenium;
+using selenium_csharp_ui_automation_framework.PageAction;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,14 +14,7 @@ namespace selenium_csharp_ui_automation_framework.Pages
 
         public bool isTitleProductDisplayed()
         {
-            try
-            {
-                return this.driver.FindElement(titleProduct).Displayed;
-            }
-            catch (NoSuchElementException)
-            {
-                return false;
-            }
+          return PageActions.isElementDisplayed(this.driver.FindElement(titleProduct));
         }
         public void verifyThatTitleProductIsDisplayed()
         {

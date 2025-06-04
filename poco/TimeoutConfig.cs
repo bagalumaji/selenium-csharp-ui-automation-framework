3b4e5f3a7e28616612f55c31c46e9d16601a9325
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-04 10:52:35 +0530

    added timeoutconfig poco

---
+++ poco/TimeoutConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace selenium_csharp_ui_automation_framework.poco
+{
+    public class TimeoutConfig
+    {
+        public int MinimumTimeout { get; set; }
+        public int DefaultTimeout{get;set;}
+        public int MaximumTimeout{ get; set; }
+    }
+}

493255ce96bc07587a1f49ea0907f3c3eb2c0b6a
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-04 12:16:08 +0530

    added code read json file

--- config/ConfigReader.cs
+++ config/ConfigReader.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Configuration;
+using selenium_csharp_ui_automation_framework.Constants;
+using selenium_csharp_ui_automation_framework.exceptions;
+using selenium_csharp_ui_automation_framework.poco;
 
 namespace selenium_csharp_ui_automation_framework.config
 {
-    internal class ConfigReader
+    public static class ConfigReader
     {
+        private static IConfiguration _configReader;
+
+        static ConfigReader()
+        {
+
+            if (!File.Exists(FrameworkConstants.configFilePath))
+            {
+                throw new ConfigFileNotFoundException($"Missing config file at: {FrameworkConstants.configFilePath}");
+            }
+            _configReader = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(FrameworkConstants.configFilePath, optional: false, reloadOnChange: true)
+                .Build();
+           
+        }
+
+        public static Config GetTimeoutConfig()
+        {
+            var timeoutConfig = _configReader.Get<Config>();
+            if (timeoutConfig == null)
+            {
+                throw new ConfigFileNotFoundException(FrameworkConstants.configFilePath + " is missing the configuration file.");
+            }
+            return timeoutConfig;
+        }
+
     }
+
 }

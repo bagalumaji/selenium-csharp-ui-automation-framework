487480c65de22cd10c82fd6537e0ad6eac3a7deb
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-04 12:38:12 +0530

    updated timeout with config values

--- Driver/Driver.cs
+++ Driver/Driver.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using selenium_csharp_ui_automation_framework.config;
 
 namespace selenium_csharp_ui_automation_framework.driver
 {
@@ -11,11 +12,12 @@ namespace selenium_csharp_ui_automation_framework.driver
         {
             if (DriverManager.GetDriver() == null)
             {
+                var config = ConfigReader.GetConfig();
                 IWebDriver driver = new ChromeDriver();
                 driver.Manage().Window.Maximize();
-                driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
-                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-                driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+                driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(config.Timeout.PageLoadTimeout);
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(config.Timeout.MinimumTimeout);
+                driver.Navigate().GoToUrl(config.Url);
                 DriverManager.SetDriver(driver);
             }
 

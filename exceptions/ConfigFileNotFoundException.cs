192e318d3be2195d19e9fbe2babab82169788060
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-04 12:16:41 +0530

    added custome exception for config file

---
+++ exceptions/ConfigFileNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace selenium_csharp_ui_automation_framework.exceptions
+{
+    public class ConfigFileNotFoundException : Exception
+    {
+        public ConfigFileNotFoundException(string message) :base(message){ }
+    }
+}

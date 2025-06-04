b4b08edbed35f2c6d46707457437b33f0da285c8
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-04 12:29:52 +0530

    updated code

--- config/ConfigReader.cs
+++ config/ConfigReader.cs
@@ -7,32 +7,25 @@ namespace selenium_csharp_ui_automation_framework.config
 {
     public static class ConfigReader
     {
-        private static IConfiguration _configReader;
+        private static readonly IConfiguration _configReader;
+        private static readonly Config _cachedConfig;
 
         static ConfigReader()
         {
-
             if (!File.Exists(FrameworkConstants.configFilePath))
             {
                 throw new ConfigFileNotFoundException($"Missing config file at: {FrameworkConstants.configFilePath}");
             }
+
             _configReader = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile(FrameworkConstants.configFilePath, optional: false, reloadOnChange: true)
                 .Build();
-           
+
+            _cachedConfig = _configReader.Get<Config>()
+                ?? throw new ConfigFileNotFoundException("Failed to load config into POCO. File might be not well structured...");
         }
 
-        public static Config GetTimeoutConfig()
-        {
-            var timeoutConfig = _configReader.Get<Config>();
-            if (timeoutConfig == null)
-            {
-                throw new ConfigFileNotFoundException(FrameworkConstants.configFilePath + " is missing the configuration file.");
-            }
-            return timeoutConfig;
-        }
-
+        public static Config GetTimeoutConfig() => _cachedConfig;
     }
-
 }

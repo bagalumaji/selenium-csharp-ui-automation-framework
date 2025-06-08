c99e2b89e95dfb5944ab5458d72b256b27d20ae5
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-08 17:29:42 +0530

    added WaitForVisibilityOfElement methhod

--- factory/ExplicitWaitFactory.cs
+++ factory/ExplicitWaitFactory.cs
@@ -1,6 +1,21 @@
-﻿namespace selenium_csharp_ui_automation_framework.factory
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using selenium_csharp_ui_automation_framework.config;
+using selenium_csharp_ui_automation_framework.driver;
+using SeleniumExtras.WaitHelpers;
+namespace selenium_csharp_ui_automation_framework.factory
 {
     public class ExplicitWaitFactory
     {
+        private static WebDriverWait getExplicitWait(int timeoutInSeconds)
+        {
+            return new WebDriverWait(DriverManager.GetDriver(), TimeSpan.FromSeconds(timeoutInSeconds));
+        }
+
+        public static IWebElement WaitForVisibilityOfElement(By by, int? timeoutInSeconds = null)
+        {
+            return getExplicitWait(timeoutInSeconds ?? ConfigReader.GetConfig().Timeout.DefaultTimeout)
+                .Until(ExpectedConditions.ElementIsVisible(by));
+        }
     }
 }

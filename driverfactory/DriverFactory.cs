a07ba9bf7f89c78444f35959b6163cd9b2c164d2
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-08 17:07:06 +0530

    removed unused imports

--- driverfactory/DriverFactory.cs
+++ driverfactory/DriverFactory.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace selenium_csharp_ui_automation_framework.driverfactory
+﻿namespace selenium_csharp_ui_automation_framework.driverfactory
 {
     internal class DriverFactory
     {

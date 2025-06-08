feeb0f2e5e94270a88c49c62a92e3d72061b64fd
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-08 17:11:46 +0530

    removed removed unused imports

--- factory/ExplicitWaitFactory.cs
+++ factory/ExplicitWaitFactory.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace selenium_csharp_ui_automation_framework.factory
+﻿namespace selenium_csharp_ui_automation_framework.factory
 {
-    internal class ExplicitWaitFactory
+    public class ExplicitWaitFactory
     {
     }
 }

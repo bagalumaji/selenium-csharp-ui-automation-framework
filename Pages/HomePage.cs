31af97effad1761bcf2e79d0a755db0616583aaa
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-09 22:11:17 +0530

    removed unued imports

--- Pages/HomePage.cs
+++ Pages/HomePage.cs
@@ -1,10 +1,5 @@
 ﻿using OpenQA.Selenium;
 using selenium_csharp_ui_automation_framework.PageAction;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace selenium_csharp_ui_automation_framework.Pages
 {
@@ -14,11 +9,11 @@ namespace selenium_csharp_ui_automation_framework.Pages
 
         public bool IsTitleProductDisplayed()
         {
-          return TitleProduct.IsElementDisplayed();
+            return TitleProduct.IsElementDisplayed();
         }
         public void VerifyThatTitleProductIsDisplayed()
         {
-            Assert.That(IsTitleProductDisplayed(), "Title 'Products' is not displayed on the page.");   
+            Assert.That(IsTitleProductDisplayed(), "Title 'Products' is not displayed on the page.");
         }
 
     }

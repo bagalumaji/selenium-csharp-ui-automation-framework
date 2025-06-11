52892b8ce19a2953983ded931d29c20792dc5681
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-11 10:09:26 +0530

    added inner exception

---
+++ exceptions/BrowserTypeNotSupportedException.cs
@@ -0,0 +1,14 @@
+﻿namespace selenium_csharp_ui_automation_framework.exceptions
+{
+    public class BrowserTypeNotSupportedException : ArgumentException
+    {
+        public BrowserTypeNotSupportedException(string browserType)
+            : base($"Browser type '{browserType}' is not supported. Please use a supported browser types....")
+        {
+        }
+        public BrowserTypeNotSupportedException(string browserType, Exception innerException)
+          : base($"Browser type '{browserType}' is not supported.", innerException)
+        {
+        }
+    }
+}

a43e5613d9c12d552268908d93265c7d332a777b
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-11 09:09:44 +0530

    created FirefoxDriverManager

---
+++ driver/manager/FirefoxDriverManager.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace selenium_csharp_ui_automation_framework.driver.manager
+{
+    public class FirefoxDriverManager : IDriver
+    {
+        public IWebDriver GetDriver()
+        {
+            return new FirefoxDriver();
+        }
+    }
+}

4ac4383e7b6354ef089245768073abec41ca9028
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-11 09:09:32 +0530

    created EdgeDriverManager

---
+++ driver/manager/EdgeDriverManager.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+
+namespace selenium_csharp_ui_automation_framework.driver.manager
+{
+    public class EdgeDriverManager : IDriver
+    {
+        public IWebDriver GetDriver()
+        {
+            return new EdgeDriver();
+        }
+    }
+}

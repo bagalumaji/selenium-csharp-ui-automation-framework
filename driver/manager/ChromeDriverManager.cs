287429d37a9ccca133b38d9c4a4905dc3b47e37a
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-11 09:09:24 +0530

    created ChromeDriverManager

---
+++ driver/manager/ChromeDriverManager.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace selenium_csharp_ui_automation_framework.driver.manager
+{
+    public class ChromeDriverManager : IDriver
+    {
+        public IWebDriver GetDriver()
+        {
+            return new ChromeDriver();
+        }
+    }
+}

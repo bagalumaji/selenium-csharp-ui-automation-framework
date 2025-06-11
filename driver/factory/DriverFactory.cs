d6933b9335accf0129ce025d2ce56dff3e2f8cbe
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-11 09:10:08 +0530

    updated with Idriver code

--- driver/factory/DriverFactory.cs
+++ driver/factory/DriverFactory.cs
@@ -2,19 +2,20 @@
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Firefox;
+using selenium_csharp_ui_automation_framework.driver.manager;
 
 namespace selenium_csharp_ui_automation_framework.driver.factory
 {
     public static class DriverFactory
     {
 
-    public static IWebDriver CreateDriver(string browserName)
+    public static IDriver CreateDriver(string browserName)
         {
             return browserName.ToLower() switch
             {
-                "chrome" => new ChromeDriver(),
-                "firefox" => new FirefoxDriver(),
-                "edge" => new EdgeDriver(),
+                "chrome" => new ChromeDriverManager(),
+                "firefox" => new FirefoxDriverManager(),
+                "edge" => new EdgeDriverManager(),
                 _ => throw new ArgumentException($"Browser '{browserName}' is not supported.")
             };
         }

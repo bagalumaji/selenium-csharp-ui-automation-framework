d4244b3042918ff8c8782287fc4fd787266c638c
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-11 09:36:48 +0530

    updated

--- Tests/DemoTests.cs
+++ Tests/DemoTests.cs
@@ -1,8 +1,7 @@
-using selenium_csharp_ui_automation_framework.driver;
-using selenium_csharp_ui_automation_framework.Pages;
-using selenium_csharp_ui_automation_framework.Tests.TestSetup;
 using selenium_csharp_ui_automation_framework.config;
 using selenium_csharp_ui_automation_framework.constants;
+using selenium_csharp_ui_automation_framework.Pages;
+using selenium_csharp_ui_automation_framework.Tests.TestSetup;
 namespace selenium_csharp_ui_automation_framework.Tests
 {
     public class DemoTests : BaseTestSetup

1fe357c07d8c2420a17cc3826cd4f2f84f5cfcb6
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-12 11:02:06 +0530

    updated code

--- driver/factory/DriverFactory.cs
+++ driver/factory/DriverFactory.cs
@@ -1,23 +1,25 @@
-﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Edge;
-using OpenQA.Selenium.Firefox;
-using selenium_csharp_ui_automation_framework.driver.manager;
+﻿using selenium_csharp_ui_automation_framework.driver.manager;
+using selenium_csharp_ui_automation_framework.enums;
+using selenium_csharp_ui_automation_framework.exceptions;
 
 namespace selenium_csharp_ui_automation_framework.driver.factory
 {
     public static class DriverFactory
     {
-
-    public static IDriver CreateDriver(string browserName)
+        private static readonly Dictionary<BrowserTypes, IDriver> _drivers = new()
         {
-            return browserName.ToLower() switch
-            {
-                "chrome" => new ChromeDriverManager(),
-                "firefox" => new FirefoxDriverManager(),
-                "edge" => new EdgeDriverManager(),
-                _ => throw new ArgumentException($"Browser '{browserName}' is not supported.")
-            };
+            { BrowserTypes.CHROME,new ChromeDriverManager()},
+            { BrowserTypes.FIREFOX, new FirefoxDriverManager() },
+            { BrowserTypes.EDGE, new EdgeDriverManager() }
+        };
+
+
+        public static IDriver CreateDriver(BrowserTypes browserType)
+        {
+            return _drivers.TryGetValue(browserType, out var driver)
+                ? driver : throw new BrowserTypeNotSupportedException($"Browser type '{browserType.ToString()}' is not supported....");
+
+
         }
     }
 }

9de65be88b5a725c8d8f4b4d823d0e7b1ff969d5
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-13 11:01:08 +0530

    refactored class name to ProductsPage

---
+++ pages/ProductsPage.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using selenium_csharp_ui_automation_framework.config;
+using selenium_csharp_ui_automation_framework.PageAction;
+
+namespace selenium_csharp_ui_automation_framework.Pages
+{
+    public class ProductsPage : BasePage
+    {
+        private static By TitleProduct => By.XPath("//span[normalize-space()='Products']");
+
+        public bool IsTitleProductDisplayed()
+        {
+            return TitleProduct.IsElementDisplayed(ConfigReader.GetConfig().Timeout.DefaultTimeout);
+        }
+        public void VerifyThatTitleProductIsDisplayed()
+        {
+            Assert.That(IsTitleProductDisplayed(), "Title 'Products' is not displayed on the page.");
+        }
+
+    }
+}

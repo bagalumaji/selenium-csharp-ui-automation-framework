a52ac4754c429847e2d6a992558236c4e20d0a37
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-13 11:37:56 +0530

    added 2 methods for managing the report

--- reports/ExtentManger.cs
+++ reports/ExtentManger.cs
@@ -1,6 +1,21 @@
-﻿namespace selenium_csharp_ui_automation_framework.reports
+﻿using AventStack.ExtentReports;
+
+namespace selenium_csharp_ui_automation_framework.reports
 {
-    internal class ExtentManger
+    public class ExtentManger
     {
+        private static readonly  ThreadLocal<ExtentTest> extentTest = new();
+
+        public static ExtentTest GetExtentTest
+        {
+            get => extentTest.Value;
+            set => extentTest.Value = value;
+        }
+
+        public static void Unload()
+        {
+            extentTest.Value = null;
+            extentTest.Dispose();
+        }
     }
 }

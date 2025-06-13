a2349359ef0dc6727aa8b5b0b05b664d111c1e1d
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-13 12:23:14 +0530

    added onetime setup and teardown

--- Tests/TestSetup/BaseTestSetup.cs
+++ Tests/TestSetup/BaseTestSetup.cs
@@ -1,8 +1,19 @@
-﻿namespace selenium_csharp_ui_automation_framework.Tests.TestSetup
+﻿using selenium_csharp_ui_automation_framework.reports;
+
+namespace selenium_csharp_ui_automation_framework.Tests.TestSetup
 {
     public class BaseTestSetup
     {
-
+        [OneTimeSetUp]
+        public void BeforeAll()
+        {
+            ExtentReport.InitReport();
+        }
+        [OneTimeTearDown]
+        public void AfterAll()
+        {
+            ExtentReport.FlushReport();
+        }
         [SetUp]
         public void Setup()
         {

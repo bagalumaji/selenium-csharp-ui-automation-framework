751b3994a118ffb1638170e968220b532b104683
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-13 11:57:15 +0530

    added html report file path

--- Constants/FrameworkConstants.cs
+++ Constants/FrameworkConstants.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace selenium_csharp_ui_automation_framework.Constants
+﻿namespace selenium_csharp_ui_automation_framework.Constants
 {
     public static class FrameworkConstants
     {
         public static readonly string basePath = Directory.GetCurrentDirectory();
         public static readonly string configFilePath = Path.Combine(basePath, "testdata", "Config.json");
+        public static readonly string htmlReportFilePath = Path.Combine(basePath, "htmlreport", "ExtentReport.html");
     }
 }

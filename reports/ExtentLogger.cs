d79333f4cae415c19c030dfbcdf850a71d0ae7ab
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-13 11:39:27 +0530

    added basic logging methods

--- reports/ExtentLogger.cs
+++ reports/ExtentLogger.cs
@@ -1,6 +1,18 @@
 ﻿namespace selenium_csharp_ui_automation_framework.reports
 {
-    internal class ExtentLogger
+    public static class ExtentLogger
     {
+        public static void Info(string message)
+        {
+            ExtentManger.GetExtentTest.Info(message);
+        }
+        public static void Pass(string message)
+        {
+            ExtentManger.GetExtentTest.Pass(message);
+        }
+        public static void Fail(string message)
+        {
+            ExtentManger.GetExtentTest.Fail(message);
+        }
     }
 }

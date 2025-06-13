db5a7431158fd745afc97c08c00513c6b6697fcb
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-13 11:05:39 +0530

    removed unused imports

--- reports/ExtentLogger.cs
+++ reports/ExtentLogger.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace selenium_csharp_ui_automation_framework.reports
+﻿namespace selenium_csharp_ui_automation_framework.reports
 {
     internal class ExtentLogger
     {

923be52ecc477002ae4b0716200a75bc8f009904
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-21 12:26:44 +0530

    updated with test case name util

--- reports/ExtentLogger.cs
+++ reports/ExtentLogger.cs
@@ -1,18 +1,13 @@
-﻿namespace selenium_csharp_ui_automation_framework.reports
+﻿using selenium_csharp_ui_automation_framework.utils;
+
+namespace selenium_csharp_ui_automation_framework.reports
 {
     public static class ExtentLogger
     {
-        public static void Info(string message)
-        {
-            ExtentManger.GetExtentTest().Info(message);
-        }
-        public static void Pass(string message)
-        {
-            ExtentManger.GetExtentTest().Pass(message);
-        }
-        public static void Fail(string message)
-        {
-            ExtentManger.GetExtentTest().Fail(message);
-        }
+        public static void Info(string message)=>ExtentManger.GetExtentTest().Info(message);
+        public static void Pass(string message)=>ExtentManger.GetExtentTest().Pass(TestCaseNameUtil.GetTestCaseName() + message);
+        public static void Pass()=> ExtentManger.GetExtentTest().Pass(TestCaseNameUtil.GetTestCaseName()+ " Test passed successfully.");
+        public static void Fail(string message) =>  ExtentManger.GetExtentTest().Fail(TestCaseNameUtil.GetTestCaseName() + message);
+        public static void Fail() => ExtentManger.GetExtentTest().Fail(TestCaseNameUtil.GetTestCaseName() + " Test Failed");
     }
 }

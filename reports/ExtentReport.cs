b6f564aa1466d060da864403ef27c4b92fcdf5bd
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-21 12:19:28 +0530

    updated extent report with null check

--- reports/ExtentReport.cs
+++ reports/ExtentReport.cs
@@ -7,32 +7,34 @@ namespace selenium_csharp_ui_automation_framework.reports
 {
     public class ExtentReport
     {
-        private static ExtentReports extent;
+        private static ExtentReports? extent = null;
 
         public static void InitReport()
         {
-            if (extent == null)
-            {
-                extent = new ExtentReports();
-                var htmlReporter = new ExtentSparkReporter(FrameworkConstants.htmlReportFilePath);
-                extent.AddSystemInfo("Environment", "QA");
-                extent.AddSystemInfo("Browser", ConfigReader.GetConfig().Browser);
-                extent.AddSystemInfo("OS", "Windows 10");
-                extent.AddSystemInfo("Framework", "Selenium C# UI Automation Framework");
-                htmlReporter.Config.DocumentTitle = "Selenium C# UI Automation Framework Report";
-                extent.AttachReporter(htmlReporter);
-            }
+            extent ??= CreateExtentReports();
         }
 
-        public static void CreateTest(string testName)
+        private static ExtentReports CreateExtentReports()
         {
-            Console.WriteLine("test case ...."+TestContext.CurrentContext.Test.Name);
-            ExtentManger.SetExtentTest(extent.CreateTest(testName));
+            extent = new ExtentReports();
+            var htmlReporter = new ExtentSparkReporter(FrameworkConstants.htmlReportFilePath);
+            extent.AddSystemInfo("Environment", "QA");
+            extent.AddSystemInfo("Browser", ConfigReader.GetConfig().Browser);
+            extent.AddSystemInfo("OS", "Windows 10");
+            extent.AddSystemInfo("Framework", "Selenium C# UI Automation Framework");
+            htmlReporter.Config.DocumentTitle = "Selenium C# UI Automation Framework Report";
+            extent.AttachReporter(htmlReporter);
+            return extent;
+        }
+        public static void CreateTest()
+        {
+            extent ??= CreateExtentReports();
+            ExtentManger.SetExtentTest(extent.CreateTest(TestContext.CurrentContext.Test.Name, TestContext.CurrentContext.Test.FullName));
         }
 
         public static void FlushReport()
         {
-            extent.Flush(); 
+            extent?.Flush();
             ExtentManger.Unload();
         }
     }

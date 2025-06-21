9eee3a01d0b9a16904462c183573a9c5ad3829c1
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-21 11:51:10 +0530

    updated

--- Tests/DemoTests.cs
+++ Tests/DemoTests.cs
@@ -1,25 +1,35 @@
 using selenium_csharp_ui_automation_framework.config;
 using selenium_csharp_ui_automation_framework.constants;
 using selenium_csharp_ui_automation_framework.Pages;
+using selenium_csharp_ui_automation_framework.reports;
 using selenium_csharp_ui_automation_framework.Tests.TestSetup;
 namespace selenium_csharp_ui_automation_framework.Tests
 {
     public class DemoTests : BaseTestSetup
     {
+       
         [Test]
         public void DemoTest()
         {
+            ExtentReport.InitReport();
             var config = ConfigReader.GetConfig();
 
             Console.WriteLine("Title : " + PageAction.PageActions.Title);
+            ExtentReport.CreateTest("Demo Test123");
+            ExtentLogger.Info("Title : " + PageAction.PageActions.Title);
+
             Assert.That(PageAction.PageActions.Title.Contains(LoginPageConstants.Title), "Title does not contain " + LoginPageConstants.Title);
+            ExtentLogger.Info("Title : " + PageAction.PageActions.Title);
 
             LoginPage loginPage = new();
             loginPage.verifyThatLoginButtonIsDisplayed();
             loginPage.login(config.Username, config.Password);
+            ExtentLogger.Info("Title : " + PageAction.PageActions.Title);
 
             ProductsPage homePage = new();
             homePage.VerifyThatTitleProductIsDisplayed();
+            ExtentLogger.Pass("Demo test passed successfully.");
+            ExtentReport.FlushReport();
         }
     }
 }

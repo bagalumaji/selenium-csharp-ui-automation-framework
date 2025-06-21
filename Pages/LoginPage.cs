533ba4a70a379a10326156692e071a5bab2875f8
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-21 12:36:34 +0530

    added logs

--- Pages/LoginPage.cs
+++ Pages/LoginPage.cs
@@ -1,6 +1,7 @@
 ﻿
 using OpenQA.Selenium;
 using selenium_csharp_ui_automation_framework.PageAction;
+using selenium_csharp_ui_automation_framework.reports;
 
 namespace selenium_csharp_ui_automation_framework.Pages
 {
@@ -12,15 +13,18 @@ namespace selenium_csharp_ui_automation_framework.Pages
 
         public void ClickLoginButton()
         {
+            ExtentLogger.Info("Clicking on Login button");
             LoginButton.ClickOnElement();
         }
 
         public void EnterUserName(string userName)
         {
+            ExtentLogger.Info("Entering username: " + userName);
             UserNameTextBox.SendKeysToElement(userName);
         }
         public void EnterPassword(string password)
         {
+            ExtentLogger.Info("Entering
             PasswordTextBox.SendKeysToElement(password);
         }
         public void login(string userName, string password)
@@ -35,6 +39,7 @@ namespace selenium_csharp_ui_automation_framework.Pages
         }
         public void verifyThatLoginButtonIsDisplayed()
         {
+            ExtentLogger.Info("Verifying that login button is displayed on the page");
             Assert.That(IsLoginButtonDisplayed(), "Login button is not displayed on the page.");
         }
     }

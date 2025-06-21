9b685ad58d41539a14a8c9caf8ac3a3287a3a448
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-21 12:27:17 +0530

    created and added test case util class with method

---
+++ utils/TestCaseNameUtil.cs
@@ -0,0 +1,7 @@
+﻿namespace selenium_csharp_ui_automation_framework.utils
+{
+    public static class TestCaseNameUtil
+    {
+        public static string GetTestCaseName() => TestContext.CurrentContext.Test.Name;
+    }
+}

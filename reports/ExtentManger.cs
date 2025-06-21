e5cc887395eb725bb46b371539e268f57e3c1ac4
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-21 12:30:38 +0530

    updated wih method expression

--- reports/ExtentManger.cs
+++ reports/ExtentManger.cs
@@ -6,15 +6,10 @@ namespace selenium_csharp_ui_automation_framework.reports
     {
         private static readonly  ThreadLocal<ExtentTest> _extentTest = new();
 
-        public static ExtentTest GetExtentTest()
-        {
-            return _extentTest.Value;
-        }
+        public static ExtentTest GetExtentTest() =>_extentTest.Value;
 
-        public static void SetExtentTest(ExtentTest extent)
-        {
-           _extentTest.Value = extent;
-        }
+        public static void SetExtentTest(ExtentTest extent)=> _extentTest.Value = extent;
+      
         public static void Unload()
         {
             if (_extentTest.Value != null && _extentTest.IsValueCreated)

d67b7fab2aa89fa0202cdf87f69d431d140cf2a0
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-06-22 10:27:05 +0530

    updated with null check

--- Driver/DriverManager.cs
+++ Driver/DriverManager.cs
@@ -5,17 +5,26 @@ namespace selenium_csharp_ui_automation_framework.driver
     public sealed class DriverManager
     {
         private DriverManager() { }
-        private static ThreadLocal<IWebDriver> threadLocalDriver = new ThreadLocal<IWebDriver>();
-        public static IWebDriver GetDriver() => threadLocalDriver.Value;
 
-        public static void SetDriver(IWebDriver driver) => threadLocalDriver.Value = driver;
+        private static ThreadLocal<IWebDriver>? threadLocalDriver = new();
+
+        public static IWebDriver? GetDriver() => threadLocalDriver?.Value;
+
+        public static void SetDriver(IWebDriver driver)
+        {
+            threadLocalDriver ??= new ThreadLocal<IWebDriver>();
+            threadLocalDriver.Value = driver;
+        }
+
         public static void UnloadDriver()
         {
-            if (threadLocalDriver.IsValueCreated && threadLocalDriver.Value != null)
+            if (threadLocalDriver?.IsValueCreated == true)
             {
-                threadLocalDriver.Value.Dispose();
+                threadLocalDriver.Value?.Dispose();
+                threadLocalDriver.Dispose();
                 threadLocalDriver = null;
             }
         }
     }
+
 }

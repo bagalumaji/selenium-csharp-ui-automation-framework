48e8c4771f34519ef070c89a07e97f6d2567a848
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-07-02 18:28:33 +0530

    added utility for test result status

---
+++ utils/TestResultReporterUtil.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework.Interfaces;
+using selenium_csharp_ui_automation_framework.reports;
+
+namespace selenium_csharp_ui_automation_framework.utils
+{
+    public static class TestResultReporterUtil
+    {
+        public static void ReportTestOutcome()
+        {
+            var status = TestContext.CurrentContext.Result.Outcome.Status;
+
+            switch (status)
+            {
+                case TestStatus.Failed:
+                    ExtentLogger.Fail(TestContext.CurrentContext.Result.Message);
+                    break;
+
+                case TestStatus.Passed:
+                    ExtentLogger.Pass();
+                    break;
+
+                case TestStatus.Skipped:
+                case TestStatus.Inconclusive:
+                    ExtentLogger.Skip();
+                    break;
+            }
+        }
+    }
+}

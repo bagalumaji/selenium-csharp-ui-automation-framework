9952b7d3d96fdb09952ba1fcecc90565f703eb06
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-07-02 18:08:20 +0530

    added method to capture screenshot

--- utils/ScreenshotUtil.cs
+++ utils/ScreenshotUtil.cs
@@ -1,7 +1,21 @@
-﻿namespace selenium_csharp_ui_automation_framework.utils
-{
-    public class ScreenshotUtil
-    {
+﻿using OpenQA.Selenium;
+using selenium_csharp_ui_automation_framework.driver;
 
+namespace selenium_csharp_ui_automation_framework.utils
+{
+    public static class ScreenshotUtil
+    {
+        public static string TakeScreenshotAsString()
+        {
+            var driver = DriverManager.GetDriver() ?? throw new InvalidOperationException("Driver is not initialized.");
+            if (driver is ITakesScreenshot screenshotDriver)
+            {
+                return screenshotDriver.GetScreenshot().AsBase64EncodedString;
+            }
+            else
+            {
+                throw new InvalidOperationException("Driver does not support screenshots.");
+            }
+        }
     }
 }

aada1049f50cefd1da2b8e5ff21ca5556527864e
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-07-02 18:17:28 +0530

    added code for add screenshot

--- reports/ExtentLogger.cs
+++ reports/ExtentLogger.cs
@@ -1,13 +1,22 @@
-﻿using selenium_csharp_ui_automation_framework.utils;
+﻿using AventStack.ExtentReports;
+using selenium_csharp_ui_automation_framework.utils;
 
 namespace selenium_csharp_ui_automation_framework.reports
 {
     public static class ExtentLogger
     {
-        public static void Info(string message)=>ExtentManger.GetExtentTest().Info(message);
-        public static void Pass(string message)=>ExtentManger.GetExtentTest().Pass(TestCaseNameUtil.GetTestCaseName() + message);
-        public static void Pass()=> ExtentManger.GetExtentTest().Pass(TestCaseNameUtil.GetTestCaseName()+ " Test passed successfully.");
-        public static void Fail(string message) =>  ExtentManger.GetExtentTest().Fail(TestCaseNameUtil.GetTestCaseName() + message);
-        public static void Fail() => ExtentManger.GetExtentTest().Fail(TestCaseNameUtil.GetTestCaseName() + " Test Failed");
+        public static void Info(string message) => ExtentManger.GetExtentTest().Info(message);
+        public static void Pass(string message) => ExtentManger.GetExtentTest().Pass(TestCaseNameUtil.GetTestCaseName() + message);
+        public static void Pass() => ExtentManger.GetExtentTest().Pass(TestCaseNameUtil.GetTestCaseName() + " Test passed successfully.");
+        public static void Fail(string message)
+        {
+            ExtentManger.GetExtentTest().Fail(MediaEntityBuilder.CreateScreenCaptureFromBase64String(ScreenshotUtil.TakeScreenshotAsString()).Build());
+            ExtentManger.GetExtentTest().Fail(TestCaseNameUtil.GetTestCaseName() + message);
+        }
+        public static void Fail()
+        {
+            ExtentManger.GetExtentTest().Fail(MediaEntityBuilder.CreateScreenCaptureFromBase64String(ScreenshotUtil.TakeScreenshotAsString()).Build());
+            ExtentManger.GetExtentTest().Fail(TestCaseNameUtil.GetTestCaseName() + " Test Failed");
+        }
     }
 }

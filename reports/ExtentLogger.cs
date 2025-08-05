0cf152cd9cb67c6a83cf9b1ffd12900b8aa0150b
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-08-05 19:45:20 +0530

    updated with null check

--- reports/ExtentLogger.cs
+++ reports/ExtentLogger.cs
@@ -5,20 +5,36 @@ namespace selenium_csharp_ui_automation_framework.reports
 {
     public static class ExtentLogger
     {
-        public static void Info(string message) => ExtentManger.GetExtentTest().Info(message);
-        public static void Pass(string message) => ExtentManger.GetExtentTest().Pass(TestCaseNameUtil.GetTestCaseName() + message);
-        public static void Pass() => ExtentManger.GetExtentTest().Pass(TestCaseNameUtil.GetTestCaseName() + " Test passed successfully.");
-        public static void Fail(string message)
+        public static void Info(string message)
         {
-            ExtentManger.GetExtentTest().Fail(MediaEntityBuilder.CreateScreenCaptureFromBase64String(ScreenshotUtil.TakeScreenshotAsString()).Build());
-            ExtentManger.GetExtentTest().Fail(TestCaseNameUtil.GetTestCaseName() + message);
-        }
-        public static void Fail()
-        {
-            ExtentManger.GetExtentTest().Fail(MediaEntityBuilder.CreateScreenCaptureFromBase64String(ScreenshotUtil.TakeScreenshotAsString()).Build());
-            ExtentManger.GetExtentTest().Fail(TestCaseNameUtil.GetTestCaseName() + " Test Failed");
+            ExtentManger.GetExtentTest()?.Info(message);
         }
 
-        public static void Skip()=> ExtentManger.GetExtentTest().Skip(TestCaseNameUtil.GetTestCaseName() + " Test skipped.");
+        public static void Pass(string message)
+        {
+            ExtentManger.GetExtentTest()?.Pass(TestCaseNameUtil.GetTestCaseName() + message);
+        }
+
+        public static void Pass()
+        {
+            ExtentManger.GetExtentTest()?.Pass(TestCaseNameUtil.GetTestCaseName() + " Test passed successfully.");
+        }
+
+        public static void Fail(string message)
+        {
+                ExtentManger.GetExtentTest()?.Fail(MediaEntityBuilder.CreateScreenCaptureFromBase64String(ScreenshotUtil.TakeScreenshotAsString()).Build());
+                ExtentManger.GetExtentTest()?.Fail(TestCaseNameUtil.GetTestCaseName() + message);
+        }
+
+        public static void Fail()
+        {
+                ExtentManger.GetExtentTest()?.Fail(MediaEntityBuilder.CreateScreenCaptureFromBase64String(ScreenshotUtil.TakeScreenshotAsString()).Build());
+                ExtentManger.GetExtentTest()?.Fail(TestCaseNameUtil.GetTestCaseName() + " Test Failed");
+        }
+
+        public static void Skip()
+        {
+            ExtentManger.GetExtentTest()?.Skip(TestCaseNameUtil.GetTestCaseName() + " Test skipped.");
+        }
     }
 }

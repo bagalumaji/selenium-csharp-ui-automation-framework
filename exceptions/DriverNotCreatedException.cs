486998f223ab712ed828bc9d978698b04a462a75
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-08-06 19:25:34 +0530

    created driver not created exception

---
+++ exceptions/DriverNotCreatedException.cs
@@ -0,0 +1,14 @@
+﻿namespace selenium_csharp_ui_automation_framework.exceptions
+{
+    public class DriverNotCreatedException : Exception
+    {
+        public DriverNotCreatedException(string browserType)
+            : base($"Browser type '{browserType}' is not supported. Please use a supported browser types....")
+        {
+        }
+        public DriverNotCreatedException(string browserType, Exception innerException)
+          : base($"Browser type '{browserType}' is not supported.", innerException)
+        {
+        }
+    }
+}

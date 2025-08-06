c28a44b7c0d89db48d64e053ae1dbe1fb50cdea9
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-08-06 19:25:52 +0530

    updated with null value

--- factory/ExplicitWaitFactory.cs
+++ factory/ExplicitWaitFactory.cs
@@ -2,19 +2,24 @@
 using OpenQA.Selenium.Support.UI;
 using selenium_csharp_ui_automation_framework.config;
 using selenium_csharp_ui_automation_framework.driver;
+using selenium_csharp_ui_automation_framework.exceptions;
 using SeleniumExtras.WaitHelpers;
 namespace selenium_csharp_ui_automation_framework.factory
 {
     public class ExplicitWaitFactory
     {
-        private static WebDriverWait getExplicitWait(int timeoutInSeconds)
+        private static WebDriverWait GetExplicitWait(int timeoutInSeconds)
         {
-            return new WebDriverWait(DriverManager.GetDriver(), TimeSpan.FromSeconds(timeoutInSeconds));
+
+            var driver = DriverManager.GetDriver();
+            return driver == null
+                ? throw new DriverNotCreatedException("WebDriver instance is null. Ensure that the driver is initialized before calling ExplicitWaitFactory.")
+                : new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
         }
 
         public static IWebElement WaitForVisibilityOfElement(By by, int? timeoutInSeconds = null)
         {
-            return getExplicitWait(timeoutInSeconds ?? ConfigReader.GetConfig().Timeout.MinimumTimeout)
+            return GetExplicitWait(timeoutInSeconds ?? ConfigReader.GetConfig().Timeout.MinimumTimeout)
                 .Until(ExpectedConditions.ElementIsVisible(by));
         }
     }
